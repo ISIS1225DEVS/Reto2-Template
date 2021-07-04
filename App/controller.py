115a84e9ca72f6c2072b8975b8f1f650103b4199
martin-2k <85652994+martin-2k@users.noreply.github.com>
2021-07-04 13:17:30 -0500

    Primera entrega-Reto 2

--- App/controller.py
+++ App/controller.py
@@ -20,8 +20,9 @@
  * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
  """
 
+from os import sep
 import config as cf
-import model
+import model 
 import csv
 
 
@@ -30,9 +31,44 @@ El controlador se encarga de mediar entre la vista y el modelo.
 """
 
 # Inicialización del Catálogo de libros
+def initialize():
+    Datos=model.initialize()
+    return Datos 
 
 # Funciones para la carga de datos
+def Load_Data(storage:dict):
+
+    Load_cetegories(storage)
+    Load_videos(storage)
+    
+
+def Load_videos(storage:dict):
+    videos_File = cf.data_dir + 'videos-large.csv'
+    input_file = csv.DictReader(open(videos_File, encoding='utf-8'))
+    for video in input_file:
+        model.add_video(storage, video)
+
+def Load_cetegories(storage:dict):
+    cat_File = cf.data_dir + 'category-id.csv'
+    input_file = csv.DictReader(open(cat_File, encoding='utf-8'), delimiter='\t')
+    for cat in input_file:
+        model.add_categoria_id(storage, cat)
 
 # Funciones de ordenamiento
 
+
 # Funciones de consulta sobre el catálogo
+def filtrar_count_cat(videos:list, categories:list, categoria:str, pais:str)->list:
+    return model.filtrar_count_cat(videos, categories, categoria, pais)
+
+def filtrar_cat_n(categories, categoria:str,n:int)->list:
+    return model.filtrar_cat_n(categories, categoria,n)
+
+def filtrar_count_tag(videos, pais, tag)->list:
+    return model.filtrar_count_tag(videos, pais, tag)
+
+def max_vids_count(vids:list,pais:str):
+    return model.max_vids_count(vids,pais)
+
+def max_vids_cat(videos:list, categories:list, categoria:str):
+    return model.max_vids_cat(videos, categories, categoria)
