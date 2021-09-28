d822b55b803145615ce372601edaf8afc7cbf9d5
Mateo López <m.lopez24@uniandes.edu.co>
2021-09-28 09:39:50 -0500

    Datos del Reto 1

--- App/controller.py
+++ App/controller.py
@@ -20,6 +20,7 @@
  * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
  """
 
+from DISClib.ADT import list as lt
 import config as cf
 import model
 import csv
@@ -28,11 +29,95 @@ import csv
 """
 El controlador se encarga de mediar entre la vista y el modelo.
 """
+def initCatalog(estructuraDatos):
+    """
+    Llama la funcion de inicializacion del catalogo del modelo.
+    """
+    catalog = model.newCatalog(estructuraDatos)
+    return catalog
 
-# Inicialización del Catálogo de libros
 
 # Funciones para la carga de datos
 
-# Funciones de ordenamiento
 
-# Funciones de consulta sobre el catálogo
+def loadData(catalog):
+    """
+    Carga los datos de los archivos y cargar los datos en la
+    estructura de datos
+    """
+    loadArt(catalog)
+    loadArtist(catalog)
+   
+
+
+def loadArt(catalog):
+   
+    artworksfile = cf.data_dir +  'Artworks-utf8-small.csv'
+    input_file = csv.DictReader(open(artworksfile , encoding='utf-8'))
+    for artwork in input_file:
+        model.addArt(catalog, artwork)
+
+
+def loadArtist(catalog):
+ 
+    artistsfile = cf.data_dir + 'Artists-utf8-small.csv'
+    input_file = csv.DictReader(open(artistsfile, encoding='utf-8'))
+    contador= 0
+    for artist in input_file:
+        """
+        if contador>=50:
+            break
+        else: 
+            model.addArtist(catalog, artist)
+            contador+=1
+        """
+        model.addArtist(catalog, artist)
+        contador+=1
+
+def conteo_artistas(artistas, inicial, final):
+    return model.get_conteo(artistas, inicial, final)
+
+def primeros_tres(artistas, inicial, final):
+    return model.get_primeros(artistas, inicial, final)
+
+def ultimos_tres(artistas, inicial, final):
+    return model.get_ultimos(artistas, inicial, final)
+
+def get_obras(catalog, nombre_artista):
+    return model.get_obrasxtecnica(catalog, nombre_artista)
+
+def get_info_transporte(arte, nombre_departamento):
+    return model.get_transporte(arte, nombre_departamento)
+
+#Requerimiento 2
+def Req2(catalog, fechai, fechaf):
+    Lista = lt.newList()
+    Compras = 0
+    for keys in catalog['Art']:   
+         listaArte = catalog['Art'][keys]
+         if type(listaArte) == list:
+            for Artworks in listaArte:
+                NewLista = model.AddArtFecha(Artworks, fechai, fechaf, Lista)
+                if model.escompra(Artworks):
+                    Compras += 1
+    size = (lt.size(NewLista))
+
+    a = NewLista['first']['next']['next']['info']
+    ainfo = (('Titulo: '+ str(a['Title'])), ('Medio: '+ str(a['Medium'])), (' Fecha: '+ str(a['Date'])), ('Dimensiones: '+ str(a['Dimensions'])))
+    b = NewLista['first']['next']['info']
+    binfo = (('Titulo: '+ str(b['Title'])), ('Medio: '+ str(b['Medium'])), (' Fecha: '+ str(b['Date'])), ('Dimensiones: '+ str(b['Dimensions'])))
+    c = NewLista['first']['info']
+    cinfo = (('Titulo: '+ str(c['Title'])), ('Medio: '+ str(c['Medium'])), (' Fecha: '+ str(c['Date'])), ('Dimensiones: '+ str(c['Dimensions'])))
+    primeras3 = (ainfo, binfo, cinfo)
+
+    d = NewLista['last']['info']
+    ultima = ((('Titulo: '+ str(d['Title'])), ('Medio: '+ str(d['Medium'])), (' Fecha: '+ str(d['Date'])), ('Dimensiones: '+ str(d['Dimensions']))))
+
+    return ((model.OrganizarFecha(NewLista)), (size), Compras, primeras3, ultima )
+
+
+
+def Req4(catalog):
+    nacionalidades = model.verID(catalog)
+    list = model.OrganizarNacionalidad(nacionalidades)
+    return list
--- App/view.py
+++ App/view.py
@@ -37,21 +37,122 @@ operación solicitada
 def printMenu():
     print("Bienvenido")
     print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("2- Listar cronológicamente los artistas")
+    print("3- Listar cronológicamente las adquisiciones")
+    print("4- Clasificar las obras de un artista por técnica")
+    print("5- Clasificar las obras por la nacionalidad de sus creadores")
+    print("6- Transportarcobras de un departamento ")
+    print("7- Proponer una nueva exposición en el museo")
+    print("0- Salir")
 
 catalog = None
 
+def initCatalog():
+    """
+    Inicializa el catalogo o
+    """
+    return controller.initCatalog(TipoEstructura)
+
+
+def loadData(catalog):
+
+    controller.loadData(catalog)
 """
 Menu principal
 """
+TipoEstructura= "SINGLE_LINKED"
 while True:
     printMenu()
     inputs = input('Seleccione una opción para continuar\n')
     if int(inputs[0]) == 1:
+        input1 = input('Seleccione una opción para continuar\n'+
+                        'Presione 1 para cargar la lista como un Array List\n' +
+                         'Presione 2 para cargar la lista como un Linked List\n')
+        if int(input1) == 1:
+            #Cargar como Array List
+            TipoEstructura = 'ARRAY_LIST'
+            print("Se ha configurado como Array List")
+            
+        elif int(input1) == 2:
+            #Cargar como Linked List
+            TipoEstructura = 'SINGLE_LINKED'
+            print("Se ha configurado como Linked List")
+                
         print("Cargando información de los archivos ....")
+        catalog = initCatalog()
+        loadData(catalog)
+        print('Artistas cargados: ' + str(lt.size(catalog['Artist'])))
+        print('Obras cargados: ' + str(lt.size(catalog['Art'])))
+        print('Ultimos 3 artistas: ') #+ str(lt.size(catalog[''])))
+        print('Ultimas 3 obras ' ) #str(lt.size(catalog[''])))
+    elif int(inputs[0]) == 3:
+        input1 = int(input('Ingrese la fecha inicial '))
+        input2 = int(input('Ingrese la fecha final '))
+        Resultado = (controller.Req2(catalog, input1, input2))
+        #print('La lista organizada es: ')
+        #print(Resultado[0])
+        print('El numero total de obras en el rango es: ')
+        print(Resultado[1])
+        print('El total de obras en el rango adquiridas por compra es: ')
+        print(Resultado[2])
+        print('Las primeras 3 obras en orden cronologico')
+        print((Resultado[3])[0])
+        print((Resultado[3])[1])
+        print((Resultado[3])[2])
+        print('Las ultima obra en orden cronologico')
+        print(Resultado[4])
+
+    elif int(inputs[0]) == 5:
+        lista = controller.Req4(catalog)
+        print('Los 10 nacionalidades con más obras son: ')
+        print(lista['first']['info'])
+        print(lista['first']['next']['info'])
+        print(lista['first']['next']['next']['info'])
+        print(lista['first']['next']['next']['next']['info'])
+        print(lista['first']['next']['next']['next']['next']['info'])
+        print(lista['first']['next']['next']['next']['next']['next']['info'])
+        print(lista['first']['next']['next']['next']['next']['next']['next']['info'])
+        print(lista['first']['next']['next']['next']['next']['next']['next']['next']['info'])
+        print(lista['first']['next']['next']['next']['next']['next']['next']['next']['next']['info'])
+        print(lista['first']['next']['next']['next']['next']['next']['next']['next']['next']['next']['info'])
 
     elif int(inputs[0]) == 2:
-        pass
+        print("Ingrese año inicial: ")
+        inicial= int(input())
+        print("Ingrese año final: ")
+        final= int(input())
+        print ("La cantidad de artistas en dicho rango son: " + str(controller.conteo_artistas(catalog['Artist'], inicial, final)))
+        print ("\nLos tres primeros artistas son: \n")
+        primeros= controller.primeros_tres(catalog['Artist'], inicial, final)
+        for i in lt.iterator(primeros):
+            print(i['DisplayName'],i['BeginDate'], i['Nationality'], i['Gender'])
+        print ("\nLos tres últimos artistas son: \n")
+        ultimos= controller.ultimos_tres(catalog['Artist'], inicial, final)
+        for i in lt.iterator(ultimos):
+            print(i['DisplayName'],i['BeginDate'], i['Nationality'], i['Gender'])
+    
+    elif int(inputs[0]) == 4:
+        print("Ingrese nombre del artista: ")
+        nombre_artista= str(input())
+        lista_respuesta= controller.get_obras(catalog, nombre_artista)
+        print("El total de obras de " + nombre_artista + " es: " + str(lt.getElement(lista_respuesta, 1)))
+        print("El total de técnicas utilizadas por " + nombre_artista + " fue: " + str(lt.getElement(lista_respuesta, 2)))
+        print("La técnica más utilizada por " + nombre_artista + " fue: " + str(lt.getElement(lista_respuesta, 3)))
+        lista_obras= lt.getElement(lista_respuesta, 4)
+        for obra in lt.iterator(lista_obras):
+            print("------------------------------------------------")
+            print("\nTítulo: " + obra['Title'])
+            print("\nAño de la obra: " + obra['Date'])
+            print("\nTécnica: " + obra['Medium'])
+            print("\nDimensiones: " + obra['Dimensions'])
+            print("\n")
+
+    
+    elif int(inputs[0]) == 6:
+        print("Ingrese nombre del departamento: ")
+        nombre_departamento= str(input())
+        lista_respuesta_departamento= controller.get_info_transporte(catalog['Art'], nombre_departamento)
+    
 
     else:
         sys.exit(0)
