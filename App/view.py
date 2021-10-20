b3c8e0e1553460a58fac7be3192ce200fad2a969
Angie Catalina Campos Perilla <ac.campos@uniandes.edu.co>
2021-10-20 10:49:59 -0500

    Cambios view.

--- App/view.py
+++ App/view.py
@@ -140,16 +140,19 @@ while True:
         fecha_inicial = input("Fecha inicial(AAAA-MM-DD): ")
         fecha_final = input("Fecha final(AAAA-MM-DD): ")
         result = listArtworkbyDate(fecha_inicial,fecha_final,catalog)
-        print("El numero total de obras en el rango especificado es: " + str(lt.size(result[0]))) 
+        print("El numero total de obras entre" + str(fecha_inicial) + " y " + str(fecha_final) + "es: " + str(lt.size(result[0]))) 
         print("El numero de obras adquiridas por compra es: " + str(result[1]))
         print('Las ultimas 3 obras en el rango son: \n')
-        for i in range(lt.size(result[0])-3,lt.size(result[0])): 
-            artwork = lt.getElement(result[0],i) 
-            printArtWork(artwork) 
-        print("\n" +"Las primeras 3 obras son: \n")
-        for i in range(1,4):
-            artwork = lt.getElement(result[0],i) 
-            printArtWork(artwork)
+        if lt.size(result[0])>0:
+            for i in range(lt.size(result[0])-3,lt.size(result[0])): 
+                artwork = lt.getElement(result[0],i) 
+                printArtWork(artwork) 
+            print("\n" +"Las primeras 3 obras son: \n")
+            for i in range(1,4):
+                artwork = lt.getElement(result[0],i) 
+                printArtWork(artwork)
+        else:
+            print("No se encontraron obras de " + str(fecha_final) + " a " + str(fecha_final))
 
     elif int(inputs[0]) == 8: 
         depto = input('Ingrese el departamento a transportar: ')
